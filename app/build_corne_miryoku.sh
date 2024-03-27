22eb7e43d2b676ba7a547f4af58d054c2dd0bdd9
Inon Prince <inon.prince@oracle.com>
2024-03-27 10:16:07 -0700

    updated corne build script

--- app/build_corne_miryoku.sh
+++ app/build_corne_miryoku.sh
@@ -1,4 +1,4 @@
 west build -d build/right -b nice_nano_v2 -- -DSHIELD='corne_right nice_view_adapter nice_view' -DZMK_CONFIG='/Users/inon/repos/miryoku_zmk/config'
 west build -d build/left -b nice_nano_v2 -- -DSHIELD='corne_left nice_view_adapter nice_view' -DZMK_CONFIG='/Users/inon/repos/miryoku_zmk/config'
-cp /Users/inon/repos/zmk/app/build/left/zephyr/zmk.uf2 ./left.uf2
-cp /Users/inon/repos/zmk/app/build/right/zephyr/zmk.uf2 ./right.uf2
+cp /Users/inon/repos/zmk/app/build/left/zephyr/zmk.uf2 ./corne_miryoku_left.uf2
+cp /Users/inon/repos/zmk/app/build/right/zephyr/zmk.uf2 ./corne_miryoku_right.uf2
